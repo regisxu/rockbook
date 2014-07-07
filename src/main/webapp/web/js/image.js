715d47c607f7da49d2c6cb34832901ccb2874d09
Regis Xu <xu.regis@gmail.com>
2014-07-07 15:49:46 +0800

    Add spinner for image upload.

--- src/main/webapp/web/js/image.js
+++ src/main/webapp/web/js/image.js
@@ -23,9 +23,37 @@ function showImages(ids) {
 }
 
 function addPhoto(input) {
+
+    document.querySelector(".img-add").style.display = "none";
+    var images = document.querySelector(".images");
+    var div = document.createElement("div");
+    div.setAttribute("class", "pic col-md-3 img-wrap");
+    images.insertBefore(div, document.querySelector(".img-add"));
+
+    var opts = {
+        lines: 10, // The number of lines to draw
+        length: 10, // The length of each line
+        width: 5, // The line thickness
+        radius: 15, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#000', // #rgb or #rrggbb or array of colors
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: '50%', // Top position relative to parent
+        left: '50%' // Left position relative to parent
+    };
+    var spinner = new Spinner(opts);
+    spinner.spin(div);
+
     upload(new FormData(document.getElementById("photo")), function(data) {
-        var images = document.querySelector(".images");
-        var div = document.createElement("div");
+        spinner.stop();
+        document.querySelector(".img-add").style.display = null;
         div.setAttribute("class", "pic col-md-3 img-wrap");
         div.setAttribute("id", data.id);
         var img = document.createElement("img");
