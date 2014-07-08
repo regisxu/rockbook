33ef76ce08b3009f55820fa8afe3cddebb7c3d17
Regis Xu <xu.regis@gmail.com>
2014-07-08 14:48:08 +0800

    Add spinner to submit button.

--- src/main/webapp/web/js/editWall.js
+++ src/main/webapp/web/js/editWall.js
@@ -60,5 +60,28 @@ function submit() {
     };
 
     request.send(JSON.stringify(data));
+
+    document.querySelector("#btn-submit").setAttribute("disabled", "disabled");
+    var opts = {
+        lines: 10, // The number of lines to draw
+        length: 8, // The length of each line
+        width: 3, // The line thickness
+        radius: 8, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#000', // #rgb or #rrggbb or array of colors
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: '50%', // Top position relative to parent
+        left: '50%' // Left position relative to parent
+    };
+    var spinner = new Spinner(opts);
+    spinner.spin(document.querySelector(".btn-spinner"));
+
 }
 
