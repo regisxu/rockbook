791e1b1b944e000f0f48fff4628026a2038c1d95
Regis Xu <xu.regis@gmail.com>
2014-07-11 15:28:36 +0800

    Move spinner opts to common.js

--- src/main/webapp/web/js/route.js
+++ src/main/webapp/web/js/route.js
@@ -2,49 +2,31 @@ var id = parseId(location.href);
 var data = {};
 
 function show() {
-    var opts = {
-        lines: 10, // The number of lines to draw
-        length: 10, // The length of each line
-        width: 5, // The line thickness
-        radius: 15, // The radius of the inner circle
-        corners: 1, // Corner roundness (0..1)
-        rotate: 0, // The rotation offset
-        direction: 1, // 1: clockwise, -1: counterclockwise
-        color: '#000', // #rgb or #rrggbb or array of colors
-        speed: 1, // Rounds per second
-        trail: 60, // Afterglow percentage
-        shadow: false, // Whether to render a shadow
-        hwaccel: false, // Whether to use hardware acceleration
-        className: 'spinner', // The CSS class to assign to the spinner
-        zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: '50%', // Top position relative to parent
-        left: '50%' // Left position relative to parent
-    };
-    var spinner = new Spinner(opts);
-    spinner.spin(document.querySelector(".detail"));
+    var spinner = new Spinner(spinner_opts.page_loading);
+    async()
+        .op("GET")
+        .url("/api/route/" + id)
+        .before(function() { spinner.spin(document.querySelector(".detail")); })
+        .success(showData)
+        .anyway(function() { spinner.stop(); })
+        .send();
+}
 
-    d3.json("/api/route/" + id, function(error, json) {
+function showData(json) {
+    data = json;
+    var title = document.querySelector(".name")
+    title.textContent = data["name"];
+    title.setAttribute("id", id);
+    document.getElementById("desc").textContent = data.desc;
+    document.getElementById("level").textContent = data.level;
+    document.getElementById("length").textContent = data.length;
+    document.getElementById("bolts").textContent = data.bolts;
+    if (data.location) {
+        var url = "http://api.map.baidu.com/staticimage?center=" + data.location.longitude + "," + data.location.latitude + "&width=300&height=200&zoom=13"
+        document.getElementById("location").innerHTML = "<img src=\"" + url + "\" height=\"150\" width=\"200\">";
+    }
 
-        spinner.stop();
-
-	    if (error) {
-            return console.warn(error);
-        }
-
-        data = json;
-        var title = document.querySelector(".name")
-        title.textContent = data["name"];
-        title.setAttribute("id", id);
-        document.getElementById("desc").textContent = data.desc;
-        document.getElementById("level").textContent = data.level;
-        document.getElementById("length").textContent = data.length;
-        document.getElementById("bolts").textContent = data.bolts;
-        if (data.location) {
-            var url = "http://api.map.baidu.com/staticimage?center=" + data.location.longitude + "," + data.location.latitude + "&width=300&height=200&zoom=13"
-            document.getElementById("location").innerHTML = "<img src=\"" + url + "\" height=\"150\" width=\"200\">";
-        }
-
-        if (data.images) {
+    if (data.images) {
         var pic = d3.select(".pics")
             .selectAll(".pic")
             .data(data.images);
@@ -58,9 +40,8 @@ function show() {
             .attr("src", function(d) { return "../api/image/" + d.id + "?size=0x400"; })
             .attr("height", 200);
 
-            gallery("gallery");
-        }
-    });
+        gallery("gallery");
+    }
 }
 
 function edit() {
@@ -68,17 +49,11 @@ function edit() {
 }
 
 function deleteRoute() {
-    var request = new XMLHttpRequest();
-    request.open("DELETE", "/api/route/" + id, true);
-    request.onload = function(event) {
-        if (request.status == 200) {
-            console.log("Success!");
-            window.location.href = "/web/start.html";
-        } else {
-            console.log("error code: ", request.status);
-        }
-    };
-    request.send();
+    async()
+        .url("/api/route/" + id)
+        .op("DELETE")
+        .success(function() { window.location.href = "/web/start.html"; })
+        .send();
 }
 
 function parseId(url) {
--- src/main/webapp/web/js/wall.js
+++ src/main/webapp/web/js/wall.js
@@ -3,25 +3,7 @@ var data = {};
 var topo = null;
 
 function show() {
-    var opts = {
-        lines: 10, // The number of lines to draw
-        length: 10, // The length of each line
-        width: 5, // The line thickness
-        radius: 15, // The radius of the inner circle
-        corners: 1, // Corner roundness (0..1)
-        rotate: 0, // The rotation offset
-        direction: 1, // 1: clockwise, -1: counterclockwise
-        color: '#000', // #rgb or #rrggbb or array of colors
-        speed: 1, // Rounds per second
-        trail: 60, // Afterglow percentage
-        shadow: false, // Whether to render a shadow
-        hwaccel: false, // Whether to use hardware acceleration
-        className: 'spinner', // The CSS class to assign to the spinner
-        zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: '50%', // Top position relative to parent
-        left: '50%' // Left position relative to parent
-    };
-    var spinner = new Spinner(opts);
+    var spinner = new Spinner(spinner_opts.page_loading);
 
     async()
         .op("GET")
