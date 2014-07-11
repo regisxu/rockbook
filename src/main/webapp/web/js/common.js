eb7539a1243742d3048b05a7817150f3c5b85b09
Regis Xu <xu.regis@gmail.com>
2014-07-11 15:11:22 +0800

    function 'chain' could iterator object properties if there is no function names specified.

--- src/main/webapp/web/js/common.js
+++ src/main/webapp/web/js/common.js
@@ -17,84 +17,82 @@ function async() {
         config[name] = value;
     }
 
-    var op = function(value) {
-        set("op", value);
-    };
-
-    var url = function(value) {
-        set("url", value);
-    };
-
-    var data = function(value) {
-        set("data", value);
-    };
-
-    var before = function(value) {
-        set("before", value);
-    };
-
-    var after = function(value) {
-        set("after", value);
-    };
-
-    var success = function(value) {
-        set("success", value);
-    };
-
-    var fail = function(value) {
-        set("fail", value);
-    };
-
-    var anyway = function(value) {
-        set("anyway", value);
-    };
-
-    var send = function() {
-
-        var request = new XMLHttpRequest();
-        request.open(config.op, config.url, true);
-        if ("PUT" == config.op || "POST" == config.op) {
-            request.setRequestHeader("Content-Type", "application/json");
-        } else if ("GET" == config.op) {
-            request.setRequestHeader("Accept-Type", "application/json");
-        }
-        request.onload = function(event) {
-            if (request.status == 200) {
-                console.log("Success!");
-                var result = request.responseText ? JSON.parse(request.responseText) : null;
-                config.success(result);
-            } else {
-                console.log("error code: ", request.status);
-                config.fail(event);
-            }
-            config.anyway();
-        };
-
-        config.before();
-        if ("PUT" == config.op || "POST" == config.op) {
-            request.send(JSON.stringify(data));
-        } else if ("GET" == config.op || "DELETE" == config.op) {
-            request.send();
-        }
-        config.after();
-    };
-
     return chain({
-        op : op,
-        url : url,
-        data : data,
-        before : before,
-        after : after,
-        success : success,
-        fail : fail,
-        anyway : anyway,
-        send : send
-    }, ["op", "url", "data", "before", "after", "success", "fail", "anyway"]);
+        op : function(value) {
+            set("op", value);
+        },
+
+        url : function(value) {
+            set("url", value);
+        },
+
+        data : function(value) {
+            set("data", value);
+        },
+
+        before : function(value) {
+            set("before", value);
+        },
+
+        after : function(value) {
+            set("after", value);
+        },
+
+        success : function(value) {
+            set("success", value);
+        },
+
+        fail : function(value) {
+            set("fail", value);
+        },
+
+        anyway : function(value) {
+            set("anyway", value);
+        },
+
+        send : function() {
+
+            var request = new XMLHttpRequest();
+            request.open(config.op, config.url, true);
+            if ("PUT" == config.op || "POST" == config.op) {
+                request.setRequestHeader("Content-Type", "application/json");
+            } else if ("GET" == config.op) {
+                request.setRequestHeader("Accept-Type", "application/json");
+            }
+            request.onload = function(event) {
+                if (request.status == 200) {
+                    console.log("Success!");
+                    var result = request.responseText ? JSON.parse(request.responseText) : null;
+                    config.success(result);
+                } else {
+                    console.log("error code: ", request.status);
+                    config.fail(event);
+                }
+                config.anyway();
+            };
+
+            config.before();
+            if ("PUT" == config.op || "POST" == config.op) {
+                request.send(JSON.stringify(data));
+            } else if ("GET" == config.op || "DELETE" == config.op) {
+                request.send();
+            }
+            config.after();
+        }
+    });
 }
 
 function chain(obj, fnames) {
-    for (var i = 0; i < fnames.length; ++i) {
-        obj[fnames[i]] = _chain(obj[fnames[i]], obj);
+    if (!fnames) {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                obj[key] = _chain(obj[key], obj);
+            }
+        }
+    } else {
+        for (var i = 0; i < fnames.length; ++i) {
+            obj[fnames[i]] = _chain(obj[fnames[i]], obj);
+        }
     }
     return obj;
 }
