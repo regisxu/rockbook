ca979f073cbfdfae15e77806b1386c3abd822bba
Regis Xu <xu.regis@gmail.com>
2014-11-17 11:01:56 +0800

    Unified edit area in one page.

--- web/src/main/webapp/js/editArea.js
+++ web/src/main/webapp/js/editArea.js
@@ -1,5 +1,7 @@
 var id = parseId(location.href);
 var position = {};
+var data = {};
+var photos = {};
 
 function show() {
     async()
@@ -10,7 +12,7 @@ function show() {
 }
 
 function showData(json) {
-    var data = json;
+    data = json;
     var title = document.querySelector(".name")
     title.textContent = "Edit area " + data.name;
     title.setAttribute("id", id);
@@ -21,9 +23,45 @@ function showData(json) {
 
     document.getElementById("desc").textContent = data.desc ? data.desc : "";
 
+    showSubList();
+
     if (data.images) {
-        showImages(data.images.map(function(e) { return e.id; }));
+        photos = new Images(data.images.map(function(e) { return e.id; }), ".images");
+        photos.show();
     }
+}
+
+function showSubList() {
+    var subs = data.walls ? (data.areas ? data.walls.concat(data.areas) : data.walls) : (data.areas ? data.areas : null);
+
+    if (subs) {
+
+        var sub = d3.select(".subs-list tbody").selectAll("tr").data(subs, function(d) { return d.id; });
+        sub.enter()
+            .append(function() { return document.querySelector(".template .tr-sub").cloneNode(true); })
+            .attr("id", function(d) { return d.id; })
+            .selectAll(".td-sub").data(function(d) { return [d.name, typeOf(d.id), d.desc, "__button__"]; })
+            .text(function(d) { return d; });
+
+        sub.exit().remove();
+
+    }
+
+}
+
+function typeOf(id) {
+    for (var i = 0; data.walls && i < data.walls.length; ++i) {
+        if (id == data.walls[i].id) {
+            return "wall";
+        }
+    }
+
+    for (var i = 0; data.areas && i < data.areas.length; ++i) {
+        if (id == data.areas[i].id) {
+            return "area";
+        }
+    }
+    // TODO handle error
 }
 
 function showLocation(location) {
@@ -43,12 +81,10 @@ function showLocation(location) {
 }
 
 function submit() {
-    var data = {};
-    data.id = id;
     data.name = document.getElementById("name").value;
     data.desc = document.getElementById("desc").value;
     data.location = position;
-    data.images = imageIds;
+    data.images = photos.ids;
 
     var spinner = new Spinner(spinner_opts.submit);
 
@@ -73,6 +109,65 @@ $("#myModal").on("shown.bs.modal", function(e) {
 
 function ok() {
     showLocation({ longitude: frames[0].current.getPosition().lng, latitude: frames[0].current.getPosition().lat});
+}
+
+function add() {
+    if (!document.querySelector(".subs-list .tr-edit")) {
+        var row = document.querySelector(".template .tr-edit").cloneNode(true);
+        document.querySelector(".subs-list tbody").appendChild(row);
+    }
+}
+
+
+function addSub() {
+    var sub = {};
+    sub.name = document.querySelector(".td-edit .name").value;
+    sub.desc = document.querySelector(".td-edit .desc").value;
+    var type = document.querySelector(".td-edit .type").value;
+    if ("wall" == type || "area" == type) {
+        async()
+            .op("POST")
+            .url(api_location + "/" + type)
+            .data(sub)
+            .success(function(newSub) {
+                removeRow();
+                if ("wall" == type) {
+                    if (!data.walls) {
+                        data.walls = [];
+                    }
+                    data.walls.push(newSub);
+                } else if ("area" == type) {
+                    if (!data.areas) {
+                        data.areas = [];
+                    }
+                    data.areas.push(newSub);
+                }
+
+                showSubList();
+            })
+            .send();
+    }
+    // TODO handle error type or make type selectable.
+}
+
+function removeRow() {
+    d3.select(".tr-edit").remove();
+}
+
+function removeSub(sub) {
+    for (var i = 0; data.walls && i < data.walls.length; ++i) {
+        if (data.walls[i].id == sub) {
+            data.walls.splice(i, 1);
+            break;
+        }
+    }
+    for (var i = 0; data.areas && i < data.areas.length; ++i) {
+        if (data.areas[i].id == sub) {
+            data.areas.splice(i, 1);
+            break;
+        }
+    }
+    showSubList();
 }
 
 show();
