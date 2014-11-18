57a38686c0249ec659ed3da4a1cbde6992892f86
Regis Xu <xu.regis@gmail.com>
2014-11-18 15:38:48 +0800

    Refactor image remove.

--- web/src/main/webapp/js/image.js
+++ web/src/main/webapp/js/image.js
@@ -1,12 +1,27 @@
+var addTemplate = "<form action='#'>\
+                     <img class='img-thumbnail' onclick='this.parentNode.querySelector(\"input\").click()' style='width: 200px; height: 200px;' src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIj48cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0ic2lsdmVyIi8+PHRleHQgdGV4dC1hbmNob3I9Im1pZGRsZSIgeD0iMTAwIiB5PSIxMDAiIHN0eWxlPSJmaWxsOmdyYXk7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LXNpemU6ODBweDtmb250LWZhbWlseTpBcmlhbCxIZWx2ZXRpY2Esc2Fucy1zZXJpZjtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj4rPC90ZXh0Pjwvc3ZnPg=='>\
+                     <input type='file' name='file' style='display:none'/>\
+</form>";
+
 function Images(data, selector) {
+
     var ids = data;
+    var root = d3.select(selector);
+
+    var imgAdd = root.append("div")
+        .attr("class", "col-md-3 img-add")
+        .html(addTemplate);
+    imgAdd.select("input")
+        .on("change", function() {
+            add(null);
+        });
+
     var show = function() {
-        var pic = d3.select(selector)
-            .selectAll(".image")
-            .data(ids);
+
+        var pic = root.selectAll(".image").data(ids, function(d) { return d; });
         var div = pic.enter()
             .insert("div", ".img-add")
-            .attr("class", "pic col-md-3 img-wrap")
+            .attr("class", "image col-md-3 img-wrap")
             .attr("id", function(d) { return d; });
         div.append("img")
             .attr("src", function(d) { return api_location + "/image/" + d + "?size=0x300"; })
@@ -16,8 +31,14 @@ function Images(data, selector) {
             .attr("class", "img-overlay")
             .append("span").append("a")
             .attr("class", "x-close")
-            .attr("href", function(d) { return "javascript:removeImage('" + d + "')"; })
-            .text("x");
+            .text("x")
+            .on("click", function(d) { return remove(d); });
+
+        pic.exit().remove();
+
+        if (!ids || ids.length == 0) {
+            root.select(".img-add").style("display", null);
+        }
     };
 
     var upload = function (form, f) {
@@ -65,16 +86,12 @@ function Images(data, selector) {
     };
 
     var remove = function(id) {
-        var img = document.getElementById(id);
-        img.parentNode.removeChild(img);
         var index = ids.indexOf(id);
         if (index != -1) {
             ids.splice(index, 1);
         }
 
-        if (!ids || ids.length == 0) {
-            d3.select(selector + " .img-add").style("display", null);
-        }
+        show();
     };
 
     var result = {
